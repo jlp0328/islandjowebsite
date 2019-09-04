dea336d9694b8ce65b1a7df2ba510ea144bcf7a3
jlp0328 <jlp0328@gmail.com>
2019-09-04 15:04:22 -0400

    Pulling in and styling data for phone in menu

--- src/components/SubMenu.vue
+++ src/components/SubMenu.vue
@@ -1,42 +1,84 @@
 <template>
   <div>
-    <div
-      v-if="this.menuCategory === 'bagels' || this.menuCategory === 'breakfast' || this.menuCategory === 'kids'"
-    >
-      <div class="item-details" v-for="details in this.itemDetails" :key="details.id">
-        <p class="item-name">{{details.item}}</p>
-        <p>{{details.price}}</p>
-      </div>
-    </div>
-    <div v-else-if="this.menuCategory === 'beverages'">
+    <h3>{{this.heading}}</h3>
+    <div class="item-details-bev" v-if="this.menuCategory === 'beverages'">
       <div v-for="details in this.itemDetails" :key="details.id">
         <p>{{details.item}}</p>
       </div>
     </div>
     <div v-else-if="this.menuCategory === 'sandwiches'">
       <div v-for="details in this.itemDetails" :key="details.id">
-        <p>{{details.item}}</p>
+        <p class="item-name">{{details.item}}</p>
+        <p>
+          <span>{{details.description}}</span>...
+          <span>{{details.price}}</span>
+        </p>
+      </div>
+    </div>
+    <div v-else-if="this.menuCategory === 'soups'">
+      <div class="item-details-soup" v-for="details in this.itemDetails" :key="details.id">
+        <p class="item-name">{{details.item}}</p>
+        <p>Cup: {{details.cup}}</p>
+        <p>Bowl: {{details.bowl}}</p>
+      </div>
+    </div>
+    <div v-else>
+      <div class="item-details-main" v-for="details in this.itemDetails" :key="details.id">
+        <p class="item-name">{{details.item}}</p>
+        <p>{{details.price}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import MenuHeadings from "../menu_headings";
+
 export default {
+  date: {
+    heading: ""
+  },
   props: ["menuCategory", "itemDetails"],
+  methods: {
+    determineHeading() {
+      return MenuHeadings[this.menuCategory.toUpperCase()];
+    }
+  },
   created() {
-    this.itemDetails.reverse();
+    if (this.menuCategory === "bagels") {
+      this.itemDetails.reverse();
+    }
+    this.heading = this.determineHeading();
     console.log(this.itemDetails);
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.item-details {
-  display: grid;
-  grid-template-columns: 80% 20%;
-  align-items: center;
-  justify-items: start;
+.item-details-bev {
+  @media only screen and (max-width: $mobile-breakpoint) {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    align-items: center;
+    justify-items: center;
+  }
+}
+.item-details-main {
+  @media only screen and (max-width: $mobile-breakpoint) {
+    display: grid;
+    grid-template-columns: 80% 20%;
+    align-items: center;
+    justify-items: start;
+  }
+}
+
+.item-details-soup {
+  @media only screen and (max-width: $mobile-breakpoint) {
+    display: grid;
+    grid-template-columns: 48% 25% 27%;
+    align-items: center;
+    justify-items: start;
+  }
 }
 
 .item-name {
---
+++ src/menu_headings.js
@@ -0,0 +1,17 @@
+export default Object({
+  BAGELS: "",
+  BEVERAGES:
+    "We have a large selection of coffee and espresso drinks that can be served hot or cold. We also carry quite a few sugar-free syrups. Additionally we offer the following beverages:",
+  BREAKFAST:
+    "Breakfast sandwiches are served on your choice of bagel, croissant, wheat, rye, or sourdough.",
+  KIDS: "Served with chips and a cookie.",
+  QUICHE: "Served with a side of fruit.",
+  SALADS:
+    "Dressing choices include: Homemade Ranch, Bleu Cheese, 1000 Island, Balsamic Vinagrette, Honey Mustard and Italian",
+  SANDWICHES:
+    "Sandwiches are served on your choice of bagel, croissant, wheat, rye, or sourdough--or try any sandwich as a bowl.",
+  SANDWICHES_SIDES: "Served with chips, pasta salad, potato salad, or slaw.",
+  SOUPS: "",
+  WRAPS:
+    "Wraps are served on your choice of wheat, spinach, or garlic herb wrap."
+});
